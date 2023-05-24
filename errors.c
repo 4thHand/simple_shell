2a361bf35303205d029b9f71f32aceaee1f652f9
Ernest Shaibu <ernestshiabu@gmail.com>
2023-05-24 00:44:39 -0700

    update on file *rs.c

--- errors.c
+++ errors.c
@@ -1,85 +1,81 @@
 #include "shell.h"
 
 /**
- * _eputs - prints an input string
- * @str: the string to be printed
- *
+ * _eputtss - prints an input string
+ * @strng: the string to be printed
  * Return: Nothing
  */
-void _eputs(char *str)
+void _eputtss(char *strng)
 {
-	int i = 0;
+	int x = 0;
 
-	if (!str)
+	if (!strng)
 		return;
-	while (str[i] != '\0')
+	while (strng[x] != '\0')
 	{
-		_eputchar(str[i]);
-		i++;
+		_eputchar(strng[x]);
+		x++;
 	}
 }
 
 /**
  * _eputchar - writes the character c to stderr
- * @c: The character to print
- *
+ * @x: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _eputchar(char c)
+int _eputchar(char x)
 {
-	static int i;
+	static int h;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (x == BUF_FLUSH || h >= WRITE_BUF_SIZE)
 	{
-		write(2, buf, i);
-		i = 0;
+		write(2, buf, h);
+		h = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (x != BUF_FLUSH)
+		buf[x++] = x;
 	return (1);
 }
 
 /**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
- *
+ * _putcd - writes the character c to given fd
+ * @x: The character to print
+ * @cd: The filedescriptor to write to
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putfd(char c, int fd)
+int _putcd(char x, int cd)
 {
-	static int i;
+	static int h;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (x == BUF_FLUSH || h >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
-		i = 0;
+		write(cd, buf, h);
+		h = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (x != BUF_FLUSH)
+		buf[h++] = x;
 	return (1);
 }
 
 /**
- * _putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
+ * _putscd - prints an input string
+ * @strng: the string to be printed
+ * @cd: the filedescriptor to write to
  * Return: the number of chars put
  */
-int _putsfd(char *str, int fd)
+int _putscd(char *strng, int cd)
 {
-	int i = 0;
+	int h = 0;
 
-	if (!str)
+	if (!strng)
 		return (0);
-	while (*str)
+	while (*strng)
 	{
-		i += _putfd(*str++, fd);
+		h += _putcd(*strng++, cd);
 	}
-	return (i);
+	return (h);
 }
